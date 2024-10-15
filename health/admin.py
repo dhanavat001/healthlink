9f49f087d9d1dee1f2cbd6d474494fdd6bcc46e1
dhanavat001 <dhanavat001@gannon.edu>
2024-10-16 01:45:11 +0300

    AI models and website finalization

--- health/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import PatientProfile, DoctorProfile, HealthRecord, AIModel, Appointment, MedicalTest
-
-admin.site.register(PatientProfile)
-admin.site.register(DoctorProfile)
-admin.site.register(HealthRecord)
-admin.site.register(AIModel)
-admin.site.register(Appointment)
-admin.site.register(MedicalTest)
